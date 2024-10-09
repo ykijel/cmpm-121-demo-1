841977c99834fd71453cbbe46bd73ae221a32681
ykijel <ykijel@ucsc.edu>
2024-10-08 20:11:45 -0700

    Step 2 Complete: Added button counter

--- src/main.ts
+++ src/main.ts
@@ -9,8 +9,18 @@ const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
+let counter: number = 0;
+const counterDiv: HTMLDivElement = document.createElement("div");
+counterDiv.textContent = `${counter} spooks`;
+app.append(counterDiv);
+
 const button: HTMLButtonElement = document.createElement("button");
 button.textContent = "👻";
 button.style.padding = "15px 80px";
 button.style.fontSize = "30px";
 app.append(button);
+
+button.addEventListener("click", function () {
+    counter++; // Increment the counter
+    counterDiv.textContent = `${counter} spooks`; // Update the display
+});

d54e3360c680bf99c89b108d035ec140b5281f54
ykijel <ykijel@ucsc.edu>
2024-10-08 20:00:15 -0700

    Step 1 Complete: Added clickable button

--- src/main.ts
+++ src/main.ts
@@ -8,3 +8,9 @@ document.title = gameName;
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+const button: HTMLButtonElement = document.createElement("button");
+button.textContent = "👻";
+button.style.padding = "15px 80px";
+button.style.fontSize = "30px";
+app.append(button);
